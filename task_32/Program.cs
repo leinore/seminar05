d7bf110c7feae6d028fcff50b437d348b230d988
leinore <116540768+leinore@users.noreply.github.com>
2022-11-29 17:10:16 +0300

    add task_32

---
+++ task_32/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы 
+// замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+
+Console.WriteLine(string.Join(",", array));
+
+for (int i = 0; i < array.Length ; i++)
+{
+    array[i]*=-1;
+}
+Console.WriteLine("Новый массив: "+ string.Join(",", array));
